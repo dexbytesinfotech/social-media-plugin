167a9a9732dc0dbf1978c86af58d19a3ceca1b7d
manish-patil-dexbytes <142904737+manish-patil-dexbytes@users.noreply.github.com>
2024-03-15 13:57:57 +0530

    update code without lib

--- lib/resources/meta/helpers/facebook.FieldGenerator.d.ts
+++
@@ -1,9 +0,0 @@
-import { CommonFacebookParams } from '../common/interfaces';
-/**
- * Builds a string representing a set of fields to be used in a query or request,
- * based on the provided parameters.
- * @param field - The main field to be included in the query.
- * @param params - An object containing boolean flags for each field that can be included.
- * @returns A string representing the fields to be included in the query.
- */
-export declare function buildFields(field: string, params: CommonFacebookParams): string;
